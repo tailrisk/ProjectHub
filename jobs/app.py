4a75c123a81378e2a53b305a09e40757615d9669
robterrin <robert.terrin@gmail.com>
2019-10-06 02:15:00 -0400

    Changed jobs to projects and employers to customers

--- jobs/app.py
+++ jobs/app.py
@@ -2,8 +2,7 @@ import sqlite3
 import datetime
 from flask import Flask, render_template, g, request, redirect, url_for
 
-PATH = 'db/jobs.sqlite'
-
+PATH = 'db/projects.sqlite'
 app = Flask(__name__)
 
 def open_connection():
@@ -31,31 +30,31 @@ def close_connection(exception):
         connection.close()
 
 @app.route('/')
-@app.route('/jobs')
-def jobs():
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
-    return render_template('index.html', jobs=jobs)
+@app.route('/projects')
+def projects():
+    projects = execute_sql('SELECT project.id, project.title, project.description, project.salary, client.id as client_id, client.name as client_name FROM project JOIN client ON client.id = project.client_id')
+    return render_template('index.html', projects=projects)
 
-@app.route('/job/<job_id>')
-def job(job_id):
-    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
-    return render_template('job.html', job=job)
+@app.route('/project/<project_id>')
+def project(project_id):
+    project = execute_sql('SELECT project.id, project.title, project.description, project.salary, client.id as client_id, client.name as client_name FROM project JOIN client ON client.id = project.client_id WHERE project.id = ?', [project_id], single=True)
+    return render_template('project.html', project=project)
 
-@app.route('/employer/<employer_id>')
-def employer(employer_id):
-    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
-    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
-    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+@app.route('/client/<client_id>')
+def client(client_id):
+    client = execute_sql('SELECT * FROM client WHERE id=?', [client_id], single=True)
+    projects = execute_sql('SELECT project.id, project.title, project.description, project.salary FROM project JOIN client ON client.id = project.client_id WHERE client.id = ?', [client_id])
+    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN client ON client.id = review.client_id WHERE client.id = ?', [client_id])
+    return render_template('client.html', client=client, projects=projects, reviews=reviews)
 
-@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
-def review(employer_id):
+@app.route('/client/<client_id>/review', methods=('GET', 'POST'))
+def review(client_id):
     if request.method == 'POST':
         review = request.form['review']
         rating = request.form['rating']
         title = request.form['title']
         status = request.form['status']
         date = datetime.datetime.now().strftime("%m/%d/%Y")
-        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
-        return redirect(url_for('employer', employer_id=employer_id))
-    return render_template('review.html', employer_id=employer_id)
+        execute_sql('INSERT INTO review (review, rating, title, date, status, client_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, client_id), commit=True)
+        return redirect(url_for('client', client_id=client_id))
+    return render_template('review.html', client_id=client_id)
